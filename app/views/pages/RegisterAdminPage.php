cf42f9b6a7ec64e2c12dd439910ed1693d1de28b
ithuyln <huylnpc05258@fpt.edu.vn>
2023-11-02 21:47:04 +0700

    [ADMIN] - Thêm Product và Category - (ITW_04)

--- app/views/pages/RegisterAdminPage.php
+++ app/views/pages/RegisterAdminPage.php
@@ -1,6 +1,6 @@
-<div class="page-wrapper">
-    <div class="page-content--bge5">
-        <div class="container">
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
             <div class="login-wrap">
                 <div class="login-content">
                     <div class="login-logo">
@@ -12,25 +12,34 @@
                         <form action="" method="post" enctype="multipart/form-data" class="needs-validation was-validated">
                             <div class="form-group">
                                 <label>Username</label>
-                                <input class="au-input au-input--full" type="text" name="name" placeholder="Username" required>
+                                <input class="form-control" type="text" name="name" placeholder="Username" required>
                                 <div class="invalid-feedback">
                                     Please enter a valid username
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label>Email Address</label>
-                                <input class="au-input au-input--full" type="email" name="email" placeholder="Email" required>
+                                <input class="form-control" type="email" name="email" placeholder="Email" required>
+                                <div class="invalid-feedback">
+                                    Please enter a valid email
+                                </div>
                             </div>
                             <div class="form-group">
                                 <label>Address</label>
-                                <input class="au-input au-input--full" type="text" name="address" placeholder="Address" required>
+                                <input class="form-control" type="text" name="address" placeholder="Address" required>
+                                <div class="invalid-feedback">
+                                    Please enter a valid address
+                                </div>
                             </div>
                             <div class="form-group">
                                 <label>Phone</label>
-                                <input class="au-input au-input--full" type="number" min="0" name="phone" placeholder="Phone" required>
+                                <input class="form-control" type="number" min="0" name="phone" placeholder="Phone" required>
+                                <div class="invalid-feedback">
+                                    Please enter a valid phone
+                                </div>
                             </div>
                             <div class="form-group">
-                                <label for="select" class=" form-control-label">Role Id</label>
+                                <label for="role_id" class="form-control-label">Role Id</label>
                                 <select name="role_id" id="role_id" class="form-control">
                                     <option value="1">User</option>
                                     <option value="0">Admin</option>
@@ -38,21 +47,27 @@
                             </div>
                             <div class="form-group">
                                 <label>Password</label>
-                                <input class="au-input au-input--full" type="password" name="password" placeholder="Password" required>
+                                <input class="form-control" type="password" name="password" placeholder="Password" required>
+                                <div class="invalid-feedback">
+                                    Please enter a valid password
+                                </div>
                             </div>
                             <div class="form-group">
-                                <label>Password Repaet</label>
-                                <input class="au-input au-input--full" type="password" name="cpassword" placeholder="Password repaet" required>
+                                <label>Password Repeat</label>
+                                <input class="form-control" type="password" name="cpassword" placeholder="Password repeat" required>
+                                <div class="invalid-feedback">
+                                    Please enter a valid password repeat
+                                </div>
                             </div>
                             <div class="form-group">
                                 <label>Avatar</label>
-                                <input class="" type="file" name="avatar" placeholder="Avatar" required>
+                                <input class="form-control-file" type="file" name="avatar" required>
                             </div>
-                            <button class="au-btn au-btn--block au-btn--green" type="submit">register</button>
+                            <button class="btn btn-success btn-block" type="submit">Register</button>
                         </form>
                         <div class="register-link">
                             <p>
-                                Already have account?
+                                Already have an account?
                                 <a href="/login/">Sign In</a>
                             </p>
                         </div>
@@ -61,5 +76,4 @@
             </div>
         </div>
     </div>
-
 </div>
