bee611e9b3386632f52d52a7527ed466500a7fb6
ithuyln <huylnpc05258@fpt.edu.vn>
2023-11-24 18:58:34 +0700

    [ADMIN] - Product variants - (ITW_34_V2)

--- app/views/blocks/footer.php
+++ app/views/blocks/footer.php
@@ -32,25 +32,6 @@
 <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<script>
-    $(document).ready(function() {
-        var chartData = <?php echo json_encode($data['chartData']); ?>;
-        new Morris.Bar({
-            element: 'column-chart',
-            data: chartData,
-            xkey: 'label',
-            ykeys: ['product_count', 'total_price'],
-            labels: ['Số lượng', 'Tổng giá']
-        });
-    });
-</script>
-<script>
-    // Dữ liệu từ PHP
-    var weeklyCounts = <?php echo json_encode($data['weeklyProductStatistics']); ?>;
-
-    // Tạo mảng các tuần và số lượng tương ứng
-    var weeks = Object.keys(weeklyCounts);
-    var counts = Object.values(weeklyCounts);
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script>
     var attr_count = 1;
@@ -61,31 +42,6 @@
         jQuery('#product_attr').append(html);
     }
 
-    // Lấy canvas element
-    var ctx = document.getElementById('myChart').getContext('2d');
-
-    // Vẽ biểu đồ cột
-    var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: weeks,
-            datasets: [{
-                label: 'Số lượng sản phẩm',
-                data: counts,
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-    });
-</script>
     function remove_attr(attr_count, id) {
         jQuery.ajax({
             url: '/product/del/',
