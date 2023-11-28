10cc5e0e53bd0101e0aa2d135f15d48aae3275e2
ithuyln <huylnpc05258@fpt.edu.vn>
2023-11-28 09:18:04 +0700

    git commit -m "hotfix minhtoan"

--- app/views/resource/comment/list.php
+++ app/views/resource/comment/list.php
@@ -1,9 +1,5 @@
 <div class="table-data__tool">
     <h3 class="title-5 m-b-35">Comment Table</h3>
-    <div class="table-data__tool-right">
-        <a href="/post/add/" class="au-btn au-btn-icon au-btn--green au-btn--small">
-            <i class="zmdi zmdi-plus"></i>add item</a>
-    </div>
 </div>
 <div class="table-responsive table-responsive-data2">
     <table class="table table-data2">
