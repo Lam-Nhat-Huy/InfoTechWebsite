442128ebd3734ecba0416eb70d4a833c0ccb52b7
ithuyln <huylnpc05258@fpt.edu.vn>
2023-12-04 13:27:01 +0700

    [CLIENT] - Foooter - (ITW_52_V1)

--- app/views/blocks/FooterClient.php
+++ app/views/blocks/FooterClient.php
@@ -4,23 +4,23 @@
         <div class="row justify-content-around">
             <div class="col-sm-6 col-lg-2">
                 <div class="single_footer_part">
-                    <h4>Top Products</h4>
+                    <h4>Contact</h4>
                     <ul class="list-unstyled">
-                        <li><a href="">Managed Website</a></li>
-                        <li><a href="">Manage Reputation</a></li>
-                        <li><a href="">Power Tools</a></li>
-                        <li><a href="">Marketing Service</a></li>
+                        <li><a href="">Facebook</a></li>
+                        <li><a href="">Email</a></li>
+                        <li><a href="">Phone: 0393379824</a></li>
+                        <li><a href="">Zalo: 0393379824 </a></li>
                     </ul>
                 </div>
             </div>
             <div class="col-sm-6 col-lg-2">
                 <div class="single_footer_part">
-                    <h4>Quick Links</h4>
+                    <h4>Products</h4>
                     <ul class="list-unstyled">
-                        <li><a href="">Jobs</a></li>
-                        <li><a href="">Brand Assets</a></li>
-                        <li><a href="">Investor Relations</a></li>
-                        <li><a href="">Terms of Service</a></li>
+                        <li><a href="">Phone</a></li>
+                        <li><a href="">Laptop</a></li>
+                        <li><a href="">Headphone</a></li>
+                        <li><a href="">Keyboard</a></li>
                     </ul>
                 </div>
             </div>
@@ -28,8 +28,8 @@
                 <div class="single_footer_part">
                     <h4>Features</h4>
                     <ul class="list-unstyled">
-                        <li><a href="">Jobs</a></li>
-                        <li><a href="">Brand Assets</a></li>
+                        <li><a href="">Return policy</a></li>
+                        <li><a href="">Warranty system</a></li>
                         <li><a href="">Investor Relations</a></li>
                         <li><a href="">Terms of Service</a></li>
                     </ul>
@@ -48,11 +48,11 @@
             </div>
             <div class="col-sm-6 col-lg-4">
                 <div class="single_footer_part">
-                    <h4>Newsletter</h4>
+                    <h4>Infotech Shop</h4>
                     <p>Heaven fruitful doesn't over lesser in days. Appear creeping
                     </p>
                     <div id="mc_embed_signup">
-                        <form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="subscribe_form relative mail_part">
+                        <form target="_blank" action="" method="get" class="subscribe_form relative mail_part">
                             <input type="email" name="email" id="newsletter-form-email" placeholder="Email Address" class="placeholder hide-on-focus" onfocus="this.placeholder = ''" onblur="this.placeholder = ' Email Address '">
                             <button type="submit" name="submit" id="newsletter-submit" class="email_icon newsletter-submit button-contactForm">subscribe</button>
                             <div class="mt-10 info"></div>
@@ -73,19 +73,9 @@
                             <script>
                                 document.write(new Date().getFullYear());
                             </script> All rights reserved | This
-                            template is made with <i class="ti-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                            template is made with by <a href="" target="_blank">Infotech</a>
                             <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                         </P>
-                    </div>
-                </div>
-                <div class="col-lg-4">
-                    <div class="footer_icon social_icon">
-                        <ul class="list-unstyled">
-                            <li><a href="#" class="single_social_icon"><i class="fab fa-facebook-f"></i></a></li>
-                            <li><a href="#" class="single_social_icon"><i class="fab fa-twitter"></i></a></li>
-                            <li><a href="#" class="single_social_icon"><i class="fas fa-globe"></i></a></li>
-                            <li><a href="#" class="single_social_icon"><i class="fab fa-behance"></i></a></li>
-                        </ul>
                     </div>
                 </div>
             </div>
