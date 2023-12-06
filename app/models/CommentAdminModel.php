28d8bf996a711d0b98d6c8064111780926b16e9c
ithuyln <huylnpc05258@fpt.edu.vn>
2023-12-06 13:06:51 +0700

    hotfix: MinhToan

--- app/models/CommentAdminModel.php
+++ app/models/CommentAdminModel.php
@@ -3,7 +3,7 @@ class CommentAdminModel extends Database
 {
     public function GetAllCommet()
     {
-        $stmt = "SELECT c.id as id, c.content as content, c.created_at as cr, u.name as username
+        $stmt = "SELECT c.id as id, c.content as content, c.date as cr, u.name as username
         FROM comments c, users u
         WHERE u.id = c.user_id";
         return $this->execute($stmt);
@@ -13,7 +13,7 @@ class CommentAdminModel extends Database
 
     public function GetCommetById($comment_id)
     {
-        $stmt = "SELECT c.id as id, c.content as content, c.created_at as cr, u.name as username, p.name as product_name
+        $stmt = "SELECT c.id as id, c.content as content, c.date as cr, u.name as username, p.name as product_name
         FROM comments c
         INNER JOIN users u on c.user_id = u.id
         INNER JOIN products p on c.product_id = p.id 
