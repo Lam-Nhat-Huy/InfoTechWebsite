0673ef41fa84257915a4ac1b06c16105f00c1111
Anh-Hoai <hoaipapc05116@fpt.edu.vn>
2023-12-14 00:00:28 +0700

    [CLIENT] - Hot fix quên mật khẩu

--- app/controllers/SendEmailController.php
+++ app/controllers/SendEmailController.php
@@ -24,48 +24,57 @@ class SendEmailController extends Controller
     {
         $this->view('LoginMasterLayout', [
             'pages' => 'ResetPasswordPage',
-
         ]);
     }
-
     public function sendEmail()
     {
-
         $email = $_POST['usersEmail'];
+        if (!isset($_POST['usersEmail'])) {
+            // Handle the case where the email is not set in the POST request
+            echo 'Email is required';
+            return;
+        }
+        $email = $_POST['usersEmail'];
+        // Check if the email exists in the database
+        if (!$this->ResetPasswordModel->isEmailExists($email)) {
+            // Handle the case where the email does not exist
+            echo 'Email does not exist';
+            return;
+        }
+        // Generate token and update database
         $token = bin2hex(random_bytes(16));
         $token_hash = hash("sha256", $token);
         $expiry = date("Y-m-d H:i:s", time() + 60 * 30);
         $this->ResetPasswordModel->UpdateToken($token_hash, $expiry, $email);
+        // Send the email
         $mail = new PHPMailer(true);
         try {
-            // Cấu hình thông tin máy chủ email và tài khoản gửi
-            $mail->CharSet = 'UTF-8';
-            $mail->isSMTP();
-            $mail->Host = 'smtp.gmail.com';  // Thay đổi nếu bạn sử dụng máy chủ email khác
-            $mail->SMTPAuth = true;
-            $mail->Username = 'phamanhhoaipl@gmail.com'; // Thay đổi bằng địa chỉ email của bạn
-            $mail->Password = 'ufdsdcmzkajihxox';  // Thay đổi bằng mật khẩu của bạn
-            $mail->SMTPSecure = 'ssl';
-            $mail->Port = 465;  // Thay đổi nếu máy chủ email của bạn yêu cầu cổng khác
-
-            // Thiết lập người gửi và người nhận
-            $mail->setFrom('phamanhhoaipl@gmail.com', 'AnhHoai');  // Thay đổi bằng địa chỉ email và tên của bạn
-            $mail->addAddress($email);  // Sử dụng địa chỉ email người nhận được truyền vào hàm
-
-            $mail->isHTML(true);
-
-            // Đặt tiêu đề và nội dung của email
-            $mail->Subject = 'Password Reset';
-            $mail->Body = 'ĐỔI MẬT KHẨU THEO LINK: <a href="http://infortechwebsite//forget/?token=' . $token . '">Tại Đây</a>';
-            $mail->send();
-            echo 'Message has been sent';
-
-
-
-
-
+            // ... (rest of the code remains the same)
+                 // Cấu hình thông tin máy chủ email và tài khoản gửi
+                 $mail->CharSet = 'UTF-8';
+                 $mail->isSMTP();
+                 $mail->Host = 'smtp.gmail.com';  // Thay đổi nếu bạn sử dụng máy chủ email khác
+                 $mail->SMTPAuth = true;
+                 $mail->Username = 'phamanhhoaipl@gmail.com'; // Thay đổi bằng địa chỉ email của bạn
+                 $mail->Password = 'ufdsdcmzkajihxox';  // Thay đổi bằng mật khẩu của bạn
+                 $mail->SMTPSecure = 'ssl';
+                 $mail->Port = 465;  // Thay đổi nếu máy chủ email của bạn yêu cầu cổng khác
+     
+                 // Thiết lập người gửi và người nhận
+                 $mail->setFrom('phamanhhoaipl@gmail.com', 'AnhHoai');  // Thay đổi bằng địa chỉ email và tên của bạn
+                 $mail->addAddress($email);  // Sử dụng địa chỉ email người nhận được truyền vào hàm
+     
+                 $mail->isHTML(true);
+     
+                 // Đặt tiêu đề và nội dung của email
+                 $mail->Subject = 'Password Reset';
+                 $mail->Body = 'ĐỔI MẬT KHẨU THEO LINK: <a href="http://infortechwebsite//forget/?token=' . $token . '">Tại Đây</a>';
+                 $mail->send();
+                 echo 'Message has been sent';
+     
+    
         } catch (Exception $e) {
-            echo "Lỗi khi gửi email: " . $mail->ErrorInfo;
+            echo "Error sending email: " . $mail->ErrorInfo;
             return false;
         }
 
